4979d1dc09bfbaa33008165efb4df0c2672b9565
silverc <nennmichadam@gmail.com>
2024-10-02 13:44:30 +0200

    so, i kinda have OCD... you mean obesity?

--- Umlaute.h
+++ Umlaute.h
@@ -5,12 +5,12 @@
 #ifndef UMLAUTE_H
 #define UMLAUTE_H
 
-constexpr unsigned char AE = static_cast<unsigned char>(142);
-constexpr unsigned char ae = static_cast<unsigned char>(132);
-constexpr unsigned char OE = static_cast<unsigned char>(153);
-constexpr unsigned char oe = static_cast<unsigned char>(148);
-constexpr unsigned char UE = static_cast<unsigned char>(154);
 constexpr unsigned char ue = static_cast<unsigned char>(129);
+constexpr unsigned char ae = static_cast<unsigned char>(132);
+constexpr unsigned char AE = static_cast<unsigned char>(142);
+constexpr unsigned char oe = static_cast<unsigned char>(148);
+constexpr unsigned char OE = static_cast<unsigned char>(153);
+constexpr unsigned char UE = static_cast<unsigned char>(154);
 constexpr unsigned char ss = static_cast<unsigned char>(225);
 
 #endif //UMLAUTE_H
